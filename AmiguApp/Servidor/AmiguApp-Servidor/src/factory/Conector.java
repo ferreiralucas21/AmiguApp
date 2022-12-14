17879b941b91f4a675a10e2e0348d16807bd1053
ferreiralucas21 <ferreiralucas212002@gmail.com>
2022-12-13 21:32:17 -0300

    Criação do Conector e da Principal

---
+++ AmiguApp/Servidor/AmiguApp-Servidor/src/factory/Conector.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Conector {
+    
+    private static Connection con;
+    
+    public static Connection getConnection() {
+        try {
+           //String que armazena a url de conexão com o banco
+           String url = "jdbs:mysql://localhost:3306/"; //Caminho do banco e porta
+           String banco = "amiguapp";                   // Nome da base
+           String usuario = "root";                     // Usuário para acesso
+           String senha = "";                           // Senha para acesso
+           
+           con = DriverManager.getConnection(url + banco, usuario, senha);
+           return con;
+           
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Verifique se o MySQL (XAMPP) está ativado!");
+            return null;
+        }
+        
+    }
+    
+}
