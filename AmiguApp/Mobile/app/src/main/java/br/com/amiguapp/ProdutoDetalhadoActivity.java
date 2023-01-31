24856d73cd704b897650dcbf84fe28d4bd4c8f8b
Julia Decker <juliadcker@gmail.com>
2023-01-31 18:01:10 -0300

    Design da tela de produto detalhado

---
+++ AmiguApp/Mobile/app/src/main/java/br/com/amiguapp/ProdutoDetalhadoActivity.java
@@ -0,0 +1,14 @@
+package br.com.amiguapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProdutoDetalhadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produto_detalhado);
+    }
+}
